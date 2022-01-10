2bd1257f1c6cc547b1b7bb57307d1e5813f9e6aa
matrentman <mikeatrentman@yahoo.com>
2022-01-10 16:02:06 -0500

    Initial commit

---
+++ src/main/java/com/empty/opencvexample/OpencvExampleApplication.java
@@ -0,0 +1,13 @@
+package com.empty.opencvexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencvExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencvExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empty/opencvexample/OpencvExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empty.opencvexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencvExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
